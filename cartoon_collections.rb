99e2d4ec441ab22ef4d4fa0ac065040d795f53f7
bonveeee <bonochieng@gmail.com>
2022-09-02 12:24:39 +0300

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,51 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
+  def roll_call_dwarves(array_dwarves)# code an argument here
+    # Your code here
+    array_dwarves.map.with_index(1) do |dwarf,index|
+      puts "#{index}. #{dwarf}"
+    end
+  end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+  # roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  def summon_captain_planet(planeteer_calls) 
+    # code an argument here
+    # Your code here
+    capital_plant= planeteer_calls.map(&:capitalize)
+    words=capital_plant.map do |word|
+      "#{word}!"
+    end
+    words
+  end
+
+  def long_planeteer_calls(words)# code an argument here
+    # Your code here
+    length_word =false
+  words.detect do |word|
+    new_word= word.length>4
+      if new_word==true
+        length_word=true
+    end
+    end
+    length_word
+  end
+
+  def find_the_cheese(array)# code an argument here
+    # the array below is here to help
+    cheese_types = ["cheddar", "gouda", "camembert"]
+
+    category= cheese_types.find.first do |item|
+      cheese_types.include?(item)
+    end
+
+    if category !=nil
+      return category
+    end
+    return nil
+     end
+
+
+
+  # short_words = ["puff", "go", "two"]
+  # planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+  # soup = ["tomato soup","camembert", "thyme",  "oyster crackers", "gouda"]
